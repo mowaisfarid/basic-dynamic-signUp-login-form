a0c2f75f4515b75c0c444cae9c765d2ef4d60c92
owais farid <mowaisfarid@gmail.com>
2020-12-16 14:16:50 +0500

    login error

--- app.js
+++ app.js
@@ -1,52 +1,63 @@
-let names = [];
-let emails = [];
-let passwords = [];
+var usersData=[];
+function signup(){
+    var name=document.getElementById("userName").value;
+    var email=document.getElementById('userEmail').value;
+    var password=document.getElementById('userpassword').value;
+    var gender=document.getElementById('gender').value;
+    var nationality=document.getElementById('Nationality').value;
 
-function signup() {
-    var username = document.getElementById("userName").value;
-    var useremail = document.getElementById('userEmail').value;
-    var userPassword = document.getElementById('userpassword').value;
-    username = username.toLowerCase();
-    useremail = useremail.toLowerCase();
-    userPassword = userPassword.toLowerCase();
-    names.push(username);
-    passwords.push(userPassword);
-    emails.push(useremail);
-    localStorage.setItem('username', JSON.stringify(names));
-    localStorage.setItem('useremail', JSON.stringify( emails));
-    localStorage.setItem('password', JSON.stringify( passwords));
-    window.location.href="./index.html"
-    return false;
-}
+    var getUser= localStorage.getItem('currentusers');
+    if(getUser==null){
+        userDetails=[];
 
-function Login() {
-    let u1=JSON.parse(localStorage.getItem("username"));
-    let u2=JSON.parse(localStorage.getItem("useremail"));
-    let u3=JSON.parse( localStorage.getItem("password"));
-var u_emails=document.getElementById('email').value;
-var u_pass = document.getElementById('password').value;
-let isFound=false;
-
-
-
-for( var i=0; i<u2.length;i++){
-    if(u_emails=== u2[i]){
-        isFound=i;
-        break;
+    }
+    else{
+        userDetails=JSON.parse(getUser)
     }
 
-}
-if(isFound===false){
-    alert('user not found!')
-
-}else if(u3[isFound]===u_pass){
-    // alert('login succes!')
-    window.location.href='./dashboard.html';
-}else{
-    alert('incorrect username or password!')
-}
+        userDetails.push({
+            userName:name,
+            userEmail:email,
+            userPassword:password,
+            userGender:gender,
+         userNationality:nationality})
 
 
+        localStorage.setItem('currentusers',JSON.stringify(userDetails));
+        location.href = "index.html";
 
-    return false;
-}
+        return false;
+    }
+
+    function loginUser() {
+        var uEmail = document.getElementById('userName').value;
+        var uPassword = document.getElementById('userpassword').value;
+        uEmail = uEmail.toLowerCase();
+        var isFound = false;
+        for (var i = 0; i < usersData.length; i++) {
+            if (usersData[i].userEmail === uEmail) {
+                isFound = i
+                localStorage.setItem('cIndex', JSON.stringify(isFound))
+                break;
+            }
+        }
+        if (isFound === false) {
+            alert("user not found")
+        }
+        else if (usersData[isFound].userPassword === uPassword) {
+            alert("login")
+            location.href = "dashboard.html";
+        }
+        else {
+            alert("email or password is incorrect")
+        }
+        return false
+    }
+    function loginSuccess() {
+        var cIndex = JSON.parse(localStorage.getItem("cIndex"));
+        document.getElementById('userName').innerHTML = "Name: " + usersData[cIndex].userName;
+        document.getElementById('email').innerHTML = "Natioality: " + usersData[cIndex].userNation;
+        document.getElementById('userpassword').innerHTML = "Phone No: " + usersData[cIndex].userEmail;
+        document.getElementById('gender').innerHTML = "Gender: " + usersData[cIndex].userGender;
+    }
+   
